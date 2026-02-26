3df5156f1a4a93567f6c6511dd5ed0bb03c9619c
Vorak Tong <viraktong197@gmail.com>
2026-02-26 23:38:34 +0700

    W4 - Download App: Update: Wrap MaterialApp with ListenableBuilder to make the whole app update automatically when color changed

--- lib/2_download_app/main.dart
+++ lib/2_download_app/main.dart
@@ -19,33 +19,40 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   int _currentIndex = 1;
 
-  final List<Widget> _pages =  [DownloadsScreen(), SettingsScreen()];
+  final List<Widget> _pages = [DownloadsScreen(), SettingsScreen()];
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: appTheme,
-      home: Scaffold(
-        body: _pages[_currentIndex],
-
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentIndex,
-          onTap: (index) {
-            setState(() {
-              _currentIndex = index;
-            });
-          },
-          selectedItemColor: currentThemeColor.color,
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Downloads'),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: 'Settings',
+    return ListenableBuilder(
+      listenable: themeColorService,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: appTheme,
+          home: Scaffold(
+            body: _pages[_currentIndex],
+            bottomNavigationBar: BottomNavigationBar(
+              currentIndex: _currentIndex,
+              onTap: (index) {
+                setState(() {
+                  _currentIndex = index;
+                });
+              },
+              selectedItemColor: currentThemeColor.color,
+              items: [
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home),
+                  label: 'Downloads',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.search),
+                  label: 'Settings',
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
